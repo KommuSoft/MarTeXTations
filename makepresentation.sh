11398a0bf255831975e7d941b3bcb5d9b0b91fe9
KommuSoft <vanonsem.willem@gmail.com>
2014-11-24 00:24:11 +1100

    temp commit (by cron)

--- makepresentation.sh
+++ makepresentation.sh
@@ -1,32 +1,41 @@
+function makeTitle () {
+	echo '			<div id="title" class="step" data-x="0" data-y="0" data-scale="4">';
+	#				<span class="try">then you should try</span>
+	echo "					<h1><center>$titl</center></h1>"
+	echo "				<span><center>$date</center></span>"
+	echo "				<span>By $auth</span>"
+	echo '			</div>';
+}
+
+function slide () {
+	thex=$((1200*$slid+1200))
+	echo "			<div id=\"slide$slid\" class=\"step slide bootstrapslide\" data-x=\"$thex\" data-y=\"0\">"
+	echo "$cnti" | pandoc -f markdown -t html
+	echo '			</div>';
+	slid=$(($slid+1))
+}
+
 cnt=$(cat)
-echo '<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=1024" />
-		<meta name="apple-mobile-web-app-capable" content="yes" />';
 titl=$(echo "$cnt" | bash scrape.sh -t)
-echo "		<title>$titl</title>"
 desc=$(echo "$cnt" | bash scrape.sh -D)
-echo "		<meta name=\"description\" content=\"$desc\"/>"
 auth=$(echo "$cnt" | bash scrape.sh -a)
 date=$(echo "$cnt" | bash scrape.sh -d)
-echo "		<meta name=\"author\" content=\"$auth\"/>"
-echo '		<link href="http://fonts.googleapis.com/css?family=Open+Sans:regular,semibold,italic,italicsemibold|PT+Sans:400,700,400italic,700italic|PT+Serif:400,700,400italic,700italic" rel="stylesheet" />';
-echo '<link href="impress.js/css/impress-demo.css" rel="stylesheet" />'
-echo '	</head>
-	<body>
-		<div class="fallback-message">
-			<p>Your browser <b>does not support the features required</b> by impress.js, so you are presented with a simplified version of this presentation.</p>
-			<p>For the best experience please use the latest <b>Chrome</b>, <b>Safari</b> or <b>Firefox</b> browser.</p>
-		</div>
-		<div id="impress">
-			<div id="title" class="step" data-x="0" data-y="0" data-scale="4">';
-#				<span class="try">then you should try</span>
-echo "					<h1><center>$titl</center></h1>"
-echo "				<span><center>$date</center></span>"
-echo "				<span>By $auth</span>"
-echo '			</div>
-		</div>'
-echo '	</body>
-</html>'
+slid=0;
+echo '<!doctype html><html lang="en"><head><meta charset="utf-8" /><meta name="viewport" content="width=1024" /><meta name="apple-mobile-web-app-capable" content="yes" />';
+echo "<title>$titl</title><meta name=\"description\" content=\"$desc\"/><meta name=\"author\" content=\"$auth\"/>"
+echo ''; #<link href="http://fonts.googleapis.com/css?family=Open+Sans:regular,semibold,italic,italicsemibold|PT+Sans:400,700,400italic,700italic|PT+Serif:400,700,400italic,700italic" rel="stylesheet" />
+echo '<link href="impress.js/css/impress-demo.css" rel="stylesheet"/><link href="custom.css" rel="stylesheet"/></head><body><div class="fallback-message"><p>Your browser <b>does not support the features required</b> by impress.js, so you are presented with a simplified version of this presentation.</p><p>For the best experience please use the latest <b>Chrome</b>, <b>Safari</b> or <b>Firefox</b> browser.</p></div><div id="impress">'
+makeTitle
+for slb in `echo "$cnt" | grep -n -e '----' | cut -f1 -d:`
+do
+	slb=$(($slb+1))
+	cnti=$(echo "$cnt" | tail -n +$slb)
+	sle=$(echo "$cnti" | grep -n -m 1 -e '----' | cut -f1 -d:)
+	if [ -n "$sle" ]
+	then
+		sle=$(($sle-1))
+		cnti=$(echo "$cnti" | head -n $sle)
+	fi
+	slide
+done
+echo '</div><script src="impress.js/js/impress.js"></script><script>impress().init();</script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script></body></html>'
--- toypresentation.md
+++ toypresentation.md
@@ -1,6 +1,23 @@
-    title: MarkTeXtation
+    title: MarTeXtation
     author: Willem Van Onsem
     email: Willem.VanOnsem@cs.kuleuven.be
     date: November 23, 2014
     description: A toy presentation, to demonstrate how to organize a presentation.
 
+----
+
+# What is MarTeXTation?
+
+ - Ease creation of presentation making.
+ - GUI too much effort, not portable
+ - Solution: convert markdown to "binaries" (HTM*, TeXPDF, ...)
+ 
+----
+
+# Main focus on scientific presentations
+
+ - TeX formatted formulas
+
+----
+
+<center><strong>Any questions?</strong></center>
